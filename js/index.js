ea5faec0b0c5f01fc5a78b150a84e5a8dca8746e
peternewbury <33635041+peternewbury@users.noreply.github.com>
2018-01-24 14:58:11 +0000

    finalising html & css to start on js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
 
+	// on click all items with class of colour get assigned color randomly
+		// listen for click
+		$('#mr2c4').on('click', function() {
+			alert('hello');
+		});
+
+		// clear previous in-line styling
+
+	// pop-up quote on click
+	$('#mr3c1').on('click', function() {
+		alert('Until now, man has let himself be lulled to sleep by pathetic lyric. That is why it is so fatal for the evolution of man, for all his activities, his necessary fight for equilibrium. On the other hand it has overfilled man with tragedy: tragedy has been so overrated that people have had enough. And already this is visible, man tries to ban it from all forms of art. Thus art achieves its goal at last; it takes evolution to balance.');
+	});
 });
