2b67951453195ed1e73ff0ecd924181aa55e5448
Peter Newbury <Peter.Newbury@EF.com>
2018-01-30 20:47:44 +0000

    added videos, pop-ups and links

--- js/index.js
+++ js/index.js
@@ -1,15 +1,53 @@
 $(document).ready(function() {
 
-	// on click all items with class of colour get assigned color randomly
-		// listen for click
-		$('#mr2c4').on('click', function() {
-			alert('hello');
-		});
+	// on click all items with class of colour get assigned .colour-white
+	$('#mr2c4').on('click', function() {
+		$('.colour').toggleClass('colour-white');
+	});
 
-		// clear previous in-line styling
+	// on click all items with class of cell get assigned color randomly
+	$('#mr3c2').on('click', function() {
+		// function to list all available colours
+		var colours = ['colour-red', 'colour-blue', 'colour-yellow', 'colour-white'];
+		function changeColour() {
+		   return colours[Math.floor(Math.random() * colours.length)];
+		}
+
+		// remove any existing colour classes, 
+		$('.cell').removeClass('colour-red colour-blue colour-yellow colour-white');
+
+		// toogle random class from changeColour
+		$('.cell').addClass('colour').addClass(changeColour);
+	});
+
+	// on click popup image slideshow of design images
+	// $('#mr1c3').on('click', function() {
+	// 	$.magnificPopup.open({
+	// 	  items: {
+	// 	    src: 'https://www.youtube.com/watch?v=UOSvUfbPHpY&autoplay=1'
+	// 		  },
+	// 		  type: 'video'
+	// 		});
+	// });
+
+	$('#mr1c3').magnificPopup();
+
+
+	// on hover show link
+	$('.link').hover(
+		// mouse in add grey background and unhide
+		function() {$(this).addClass('colour-grey').children().removeClass('hidden');},
+
+		// mouse out remove grey background and hide
+		function() {$(this).removeClass('colour-grey').children().addClass('hidden');}
+	);
+
 
 	// pop-up quote on click
-	$('#mr3c1').on('click', function() {
-		alert('Until now, man has let himself be lulled to sleep by pathetic lyric. That is why it is so fatal for the evolution of man, for all his activities, his necessary fight for equilibrium. On the other hand it has overfilled man with tragedy: tragedy has been so overrated that people have had enough. And already this is visible, man tries to ban it from all forms of art. Thus art achieves its goal at last; it takes evolution to balance.');
+	$('.text').magnificPopup({
+	  type:'inline',
+	  midClick: true 
 	});
+
+
 });
