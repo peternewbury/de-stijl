56f1d204c6f9a7e17fe5ed9bcb874681b38f5a4e
Peter Newbury <Peter.Newbury@EF.com>
2018-02-01 20:34:15 +0000

    added image sliders

--- js/index.js
+++ js/index.js
@@ -20,20 +20,72 @@ $(document).ready(function() {
 		$('.cell').addClass('colour').addClass(changeColour);
 	});
 
-	// popup for images, videos and inline
+	// popup for videos and inline
 	$('.popup').magnificPopup({
 		midClick: true
 	});
 
+	// show link on hover of box
+	// check to see if using mobile device
+	if ($(window).width() >= 700){	
+		// on hover show link
+		$('.link').hover(
+			// mouse in add grey background and unhide
+			function() {$(this).addClass('colour-grey').children().removeClass('hidden');},
 
-	// on hover show link
-	$('.link').hover(
-		// mouse in add grey background and unhide
-		function() {$(this).addClass('colour-grey').children().removeClass('hidden');},
+			// mouse out remove grey background and hide
+			function() {$(this).removeClass('colour-grey').children().addClass('hidden');}
+		);
+	}	
 
-		// mouse out remove grey background and hide
-		function() {$(this).removeClass('colour-grey').children().addClass('hidden');}
-	);
 
+	// image slideshow of fashion/bulidings etc
+	$('#mr2c3').magnificPopup({
+    items: [
+      {
+        src: './images/sliders/fashion-life/buliding.jpg'
+      },
+      {
+        src: './images/sliders/fashion-life/chair.jpg'
+      },
+      {
+        src: './images/sliders/fashion-life/dress.jpg'
+      },
+      {
+        src: './images/sliders/fashion-life/socks.jpg'
+      },
+      {
+        src: './images/sliders/fashion-life/house.jpg'
+      }
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image'
+	});
+
+	// image slideshow of music
+	$('#mr4c1').magnificPopup({
+    items: [
+      {
+        src: './images/sliders/music/white-stripes.jpg'
+      },
+      {
+        src: './images/sliders/music/silverchair.jpg'
+      },
+      {
+        src: './images/sliders/music/louis-andriessen.jpg'
+      },
+      {
+        src: './images/sliders/music/nelly-van-doesberg.jpg'
+      }
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image'
+	});
 
 });
+
+			nelly-van-doesberg
